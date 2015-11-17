e9174855e9bd977531f23199f21a003405f703aa
sokoban <sokoban@naver.com>
2015-11-17 13:39:13 +0900

    Update database.py

--- Lotto/database.py
+++ Lotto/database.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import
 
 from flask.ext.sqlalchemy import SQLAlchemy
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:tnwlEkfkdgo^7@221.143.42.85/lotto645'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:7@221.143.42.85/lotto645'
 
 db = SQLAlchemy(app)
